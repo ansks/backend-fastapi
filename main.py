0f0c9d628ad6208947acf45e6942d1dac31dc59c
ansks <singhanshul31@gmail.com>
2024-08-16 22:04:44 +0400

    adding status code

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from random import randrange
 from typing import Union, Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel # Defining data schema - validation
 import numpy as np
 
 app = FastAPI()
+# Always keep in mind when structuring your APIs. It runs from top to bottom.
+# Defining Schema for Post
 
-# Defining Schema for Post 
 class Post(BaseModel):
     title: str
     content: str
@@ -22,9 +23,28 @@ my_posts = [{"title": "black tea benifits", "content": "Antioxident properties",
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/posts/{name}/")
-def get_posts(name: int):
-    return {"data": f"this is post:  {name}"}
+@app.get("/posts/latest/")
+def get_latest_post():
+    post = my_posts[len(my_posts)-1]
+    return {"detail": post}
+
+# path parameter
+@app.get("/posts/{id}/")
+def get_posts(id: int, response: Response): 
+    
+    assert type(id) == int
+    post = [entry for entry in my_posts if entry['id'] == id]
+    
+    if not post:
+        # one way
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message": f"post with id {id} not found."}
+        # other way
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                      detail=f"post with id {id} not found.")
+
+    return {"post_details": post}
+    
 
 @app.get("/posts/")
 def get_all_posts():
@@ -37,7 +57,7 @@ def get_all_posts():
     # return {"data": f"title: {payload["title"]}, content: {payload["content"]}"}
 
 # 2. Defining the schema of the input data - This will also do the data validation.
-@app.post("/posts/")
+@app.post("/posts/", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):    
     
     print(post)
@@ -48,5 +68,7 @@ def create_post(post: Post):
     post_dict["id"] = randrange(1, 10000)
     
     my_posts.append(post_dict)
+    
+    
     
     return {"data": post_dict}
