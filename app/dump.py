71371b3bfe28855fb9634cf5ef8c2e14e6cf3201
ansks <singhanshul31@gmail.com>
2024-11-24 22:08:20 +0400

    package added

--- app/dump.py
+++ app/dump.py
@@ -3,35 +3,41 @@ from sqlalchemy.orm import Session
 
 my_posts = [
     {"title": "black tea benifits", "content": "Antioxident properties", "id": 1},
-    {"title": "yellow tea benifits", "content": "oxident properties", "id": 2}]
+    {"title": "yellow tea benifits", "content": "oxident properties", "id": 2},
+]
 
 app = FastAPI()
+
+#  Write a fucntion to say hello
+
 
 @app.get("/sql_alchemy")
 def test_post_db(db: Session = Depends(get_db)):
     post = db.query(models.Post).all()
-    
-    return {"status": "success", 
-            "data": post}
-    
-    
+    # winny
+
+    return {"status": "success", "data": post}
+
+
 # 1. Defining without schema validation Method 1:
 @app.post("/create_post/")
-def create_post(payload: dict = Body(...)): 
+def create_post(payload: dict = Body(...)):
     print(payload)
     return {"data": f"title: {payload["title"]}, content: {payload["content"]}"}
 
-# 2. Defining the schema of the input data - This will also do the data validation.
+    # 2. Defining the schema of the input data - This will also do the data validation.
     print(post)
     print(post.dict())
     print(post.model_dump())
     print(f"title: {new_post.title},  content : {new_post.content}")
     post_dict = post.model_dump()
     post_dict["id"] = randrange(1, 10000)
-    
+
     my_posts.append(post_dict)
     return {"data": post_dict}
-    
+
     # Putting in database
-    # Method 1: f-string method -> exposed to SQL injection -> not advisable 
-    cur.execute(f"""INSERT INTO posts (title, content, rating) VALUES ({post.title}, {post.content}, {post.rating}) RETURNING *""")
+    # Method 1: f-string method -> exposed to SQL injection -> not advisable
+    cur.execute(
+        f"""INSERT INTO posts (title, content, rating) VALUES ({post.title}, {post.content}, {post.rating}) RETURNING *"""
+    )
--- app/main.py
+++ app/main.py
@@ -1,27 +1,25 @@
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
-from . import models
 from .database import engine
 from .routers import user, post, auth, vote
 from .config import settings
 
 
-app = FastAPI(title = "Social Media App", version="1.1.0")
+app = FastAPI(title="Social Media App", version="1.1.0")
 # Always keep in mind when structuring your APIs. It runs from top to bottom.
 
 # Creating tables in datebase
 # models.Base.metadata.create_all(bind=engine)
 
-# CORS policy allows you to make resuest from the same server 
+# CORS policy allows you to make resuest from the same server
 # By default, our API allows web browser running on same domain as our server to make request.
 
 # Java Script Console
 # fetch("http:localhost:8000").then(res => res.json()).then(console.log)
 origins = [
     "*",  # example it can be accessed from anywhere, wild card, all connections allowed.
-    # 'https://www.google.com' # Example 
+    # 'https://www.google.com' # Example
     # "https://www.youtube.com"
 ]
 
@@ -30,22 +28,17 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"], # http method , * > wild card
-    allow_headers=["*"], #
+    allow_methods=["*"],  # http method , * > wild card
+    allow_headers=["*"],  #
 )
-
 
 
 @app.get("/", tags=["Root"])
 def read_root():
     return {"Hello": "World"}
 
+
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
-
-
-
-    
-
